1a9b8122cee5d85ba4fbca95c754b6d887c11b9d
zengaffi <danja.zengaffinen@epfl.ch>
2024-12-17 14:53:28 +0100

    book regression and standardization updated

--- src/utils/regression_utils.py
+++ src/utils/regression_utils.py
@@ -8,8 +8,9 @@ import plotly.express as px
 import statsmodels.api as sm
 
 
-def multi_one_hot_encode_columns(data_df, column_names_mohe, column_names_ohe):
-    data_df = pd.get_dummies(data_df, columns=column_names_ohe, drop_first=True)
+def multi_one_hot_encode_columns(data_df, column_names_mohe, column_names_ohe = None, threshold=150):
+    if column_names_ohe: 
+        data_df = pd.get_dummies(data_df, columns=column_names_ohe, drop_first=True)
 
     for column_name in column_names_mohe:
         data_df[column_name] = data_df[column_name].apply(
@@ -20,8 +21,8 @@ def multi_one_hot_encode_columns(data_df, column_names_mohe, column_names_ohe):
         # Count the occurrences of each element
         element_counts = pd.Series([item for sublist in data_df[column_name] for item in sublist]).value_counts()
 
-        # Filter elements that appear more than 1000 times
-        frequent_elements = element_counts[element_counts > 150].index
+        # Filter elements that appear more than threshold times
+        frequent_elements = element_counts[element_counts > threshold].index
 
         # Filter the column to only include frequent elements
         data_df[column_name] = data_df[column_name].apply(
@@ -45,7 +46,7 @@ def adjust_for_inflation(data_df, target_columns, is_plotting_enabled=False):
     inflation_yearly = inflation.groupby('Year')['CPIAUCNS'].mean().reset_index()
     inflation_yearly.columns = ['Year', 'CPI']
 
-    data_df['release_date'] = pd.to_datetime(data_df['release_date'])
+    data_df['release_date'] = pd.to_datetime(data_df['release_date'], format='mixed')
     base_year = data_df.release_date.dt.year.max()
 
     data_df['movie_year'] = data_df.release_date.dt.year
@@ -61,14 +62,14 @@ def adjust_for_inflation(data_df, target_columns, is_plotting_enabled=False):
 
         if is_plotting_enabled:
             fig = px.scatter(
-                title="Original vs. Inflation-Adjusted Revenue (base year = 2013) of Movies"
+                title=f"Original vs. Inflation-Adjusted {target_column} (base year = 2013) of Movies"
             )
 
             fig.add_scatter(
                 x=data_df['movie_year'],
                 y=data_df[target_column],
                 mode='markers',
-                name='Original Revenue',
+                name=f'Original {target_column}',
                 marker=dict(color='blue'),
                 hovertext=data_df['title']
             )
@@ -138,7 +139,7 @@ def create_train_test_split(data_df, target_column, should_split_based_on_book=F
         X_train_raw, X_test_raw, y_train_raw, y_test_raw = train_test_split(X, y, test_size=test_size, random_state=seed)
 
         scaler = StandardScaler()
-        scaler.fit_transform(X_test_raw)
+        scaler.fit_transform(X_train_raw)
 
         X_train_standardized = scaler.transform(X_train_raw)
         X_test_standardized = scaler.transform(X_test_raw)
@@ -155,3 +156,43 @@ def create_train_test_split(data_df, target_column, should_split_based_on_book=F
 
     return split_dict
 
+def load_books_movies(path_final_dataset = '../data/final_dataset.csv' ):
+    # the dataframe loaded is the final dataset, see notebook books_movies_cleaning.ipynb to see how books and movies are matched
+    df_books_movies = pd.read_csv(path_final_dataset)
+    df_books_movies.drop(columns=['Unnamed: 0'], inplace=True)
+    df_books_movies.dropna(subset = ['revenue', 'budget'], inplace=True)
+    df_books_movies = df_books_movies[df_books_movies['revenue']!=0]
+    df_books_movies = df_books_movies[df_books_movies['budget']!=0]
+    df_books_movies.rename(columns={'movie_name': 'title'}, inplace=True)
+    return df_books_movies
+
+def merge_and_clean_sim(books_movies_adj):
+    similarities = pd.read_csv('../data/bookfilm_summaries_with_similarity_and_sentiment.csv')
+    merged_df = books_movies_adj.merge(similarities, how = 'left', on= 'movie_id')
+
+    # Group by 'book_id' and 'movie_id', then calculate the mean for 'similarity' and 'book_sentiment_score' to get rid of doubled columns for similarity scores
+    averaged_df = merged_df.groupby(['book_id', 'movie_id'], as_index=False).agg({
+        'similarity': 'mean',
+        'book_sentiment_score': 'mean',
+    })
+    merged_df = merged_df.drop(columns=['similarity', 'book_sentiment_score'])
+    merged_df.drop_duplicates(subset=['movie_id', 'book_id'], inplace=True)
+
+    merged_df = merged_df.merge(averaged_df, how='left', on=['book_id', 'movie_id'])
+
+    #We have 259 Nan values for the sentiment, what to do with those? drop for now
+    merged_df.dropna(subset= ['similarity'], inplace = True)
+    
+    return merged_df
+
+def books_features_preprocessing(book_sim_genres):
+    books_df = pd.read_csv('../data/books.csv')
+
+    books_df_final = book_sim_genres.merge(books_df, how = 'left', on='book_id')
+    books_df_final['part_of_series'] = books_df_final['series'].notna().astype(int)
+    rating_columns = ['one_star', 'two_stars', 'three_stars', 'four_stars', 'five_stars']
+    for col in rating_columns:
+        percentage_col = col + '_percentage'
+        books_df_final[percentage_col] = books_df_final[col] / books_df_final['rating_count']
+    books_df_final.dropna(subset = ['length'], inplace=True)
+    return books_df_final
